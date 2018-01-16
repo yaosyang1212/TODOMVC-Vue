254a2e911b3f43ecfb8050d1bd3e8066cb726902
yaosyang1212 <yangyao@yangyaodeMacBook-Pro.local>
2018-01-16 09:55:46 +0800

    todomvc-vue frist commit

---
+++ js/app.js
@@ -0,0 +1,66 @@
+; (function (vue) {
+    // 模拟假数据
+    const todos = [
+        {
+            id: 1,
+            title: 'Angular',
+            completed: false
+        },
+        {
+            id: 2,
+            title: 'Vue',
+            completed: true
+        },
+        {
+            id: 3,
+            title: 'React',
+            completed: true
+        }
+    ]
+    new Vue({
+        el: '#todoapp',
+        data: {
+            todos,
+        },
+        methods: {
+            // 添加数据
+            // addmessage:function(){
+
+            // }在ES6中可以简写成如下
+            addmessage(event) {
+                // console.log('11');
+                // console.log(event.target.value);
+                //1、获取数组对象中的每个元素
+                var text = event.target.value.trim();
+                // console.log(text);
+                
+                var id = this.todos[this.todos.length - 1].id + 1;
+                // console.log(id);
+                
+                // 每次输入都是未完成的状态
+                // 2、判断用户是否输入了文本
+                // 2.1  可以通过text的length如果为0则不做操作，
+                if (!text.length) {
+                    return
+                }
+                // 2.2  text的length的长度不为0则添加到数组中
+                this.todos.push({
+                    id,
+                    title: text,
+                    completed: false
+                })
+                event.target.value = '';
+            },
+            // 点击全选按钮进行反选
+            toggleall(event){
+                // console.log(event.target.checked);
+                // 每个的状态true和false赋值给每个li
+                this.todos.forEach(element => 
+                    element.completed = event.target.checked
+                );
+                
+            }
+
+        }
+    })
+})(Vue)
