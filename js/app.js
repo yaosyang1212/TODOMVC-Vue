58cbb3103238fea15b9f8817cc51c539e2f64bb9
yaosyang1212 <yangyao@yangyaodeMacBook-Pro.local>
2018-01-23 09:03:09 +0800

    removeID clearCompleted commit

--- js/app.js
+++ js/app.js
@@ -33,10 +33,10 @@
                 //1、获取数组对象中的每个元素
                 var text = event.target.value.trim();
                 // console.log(text);
-                
+
                 var id = this.todos[this.todos.length - 1].id + 1;
                 // console.log(id);
-                
+
                 // 每次输入都是未完成的状态
                 // 2、判断用户是否输入了文本
                 // 2.1  可以通过text的length如果为0则不做操作，
@@ -52,13 +52,35 @@
                 event.target.value = '';
             },
             // 点击全选按钮进行反选
-            toggleall(event){
+            toggleall(event) {
                 // console.log(event.target.checked);
                 // 每个的状态true和false赋值给每个li
-                this.todos.forEach(element => 
+                this.todos.forEach(element =>
                     element.completed = event.target.checked
                 );
-                
+
+            },
+            // 点击按钮删除单条数据
+            removeid(delindex) {
+                this.todos.splice(delindex, 1)
+            },
+            // 清除所有完成的
+            clearCompleted() {
+                // console.log('heheehe');
+                // 过滤未完成的数据
+                // this.todos = this.todos.filter((item)=>{
+                //     return !item.completed
+                // })可以简写成如下代码
+                // this.todos = this.todos.filter(item => !item.completed)
+
+                for (let i = 0; i < todos.length; i++) {
+                    if (this.todos[i].completed) {
+                        this.todos.splice(i, 1);
+                        i--;
+                    }
+                }
+
+
             }
 
         }
